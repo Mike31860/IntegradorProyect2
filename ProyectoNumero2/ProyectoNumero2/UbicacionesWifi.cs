f6fe1ef1644dc5c7a6ee5eeaad857d6e25425ef3
Miguel Romero <miguel980429@gmail.com>
2019-02-02 17:29:27 -0500

    Adding clases

--- ProyectoNumero2/ProyectoNumero2/UbicacionesWifi.cs
+++ ProyectoNumero2/ProyectoNumero2/UbicacionesWifi.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ProyectoNumero2
 {
+	
 	class UbicacionesWifi
 	{
+
+		private string municipio;
+		private string puntoUbicacion;
+		private string direccion;
+		private int ubicacion;
+
+		public UbicacionesWifi(string municipio, string puntoUbicacion, string direccion, int ubicacion)
+		{
+			this.Municipio = municipio;
+			this.PuntoUbicacion = puntoUbicacion;
+			this.Direccion = direccion;
+			this.Ubicacion = ubicacion;
+		}
+
+		public string Municipio { get => municipio; set => municipio = value; }
+		public string PuntoUbicacion { get => puntoUbicacion; set => puntoUbicacion = value; }
+		public string Direccion { get => direccion; set => direccion = value; }
+		public int Ubicacion { get => ubicacion; set => ubicacion = value; }
 	}
 }
